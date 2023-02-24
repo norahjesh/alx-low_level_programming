ed1ba9f22f84d7ed0abf7ee0696e574b5cd41e4e
Norah Kibathi <norahnjeri97@gmail.com>
2023-02-23 16:36:05 -0800

    main.cc

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,21 +1,15 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_most_numbers - Print numbers between 0 to 9 incl.
  *
  * Return: Void.
  */
-void print_most_numbers(void)
+void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
-	{
-		if (i != 2 && i != 4)
-		{
-			_putchar(i + '0');
-		}
-	}
+	for (i = 0; i <= 9; i++)
+		_putchar(i + '0');
 	_putchar('\n');
 }
